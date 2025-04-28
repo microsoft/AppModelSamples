92f1e112fe61cc0b733825cc68ab52ac508aaad4
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2025-04-28 17:03:16 -0400

    Removed unneeded code.

--- Samples/PackageWithExternalLocation/cs/PhotoStoreDemo/PhotosFolder.cs
+++ Samples/PackageWithExternalLocation/cs/PhotoStoreDemo/PhotosFolder.cs
@@ -9,21 +9,14 @@ namespace PhotoStoreDemo
         {
             get
             {
-                string path = null;
-                if (ExecutionMode.IsRunningWithIdentity())
+                string path = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
+                path = Path.Combine(path, "Photos");
+                var di = new DirectoryInfo(path);
+                if (!di.Exists)
                 {
-                    path = ExecutionMode.GetSafeAppxLocalFolder();
+                    di.Create();
                 }
-                if (path == null)
-                {
-                    path = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
-                    path = Path.Combine(path, "Photos");
-                    var di = new DirectoryInfo(path);
-                    if (!di.Exists)
-                    {
-                        di.Create();
-                    }
-                }
+
                 return path;
             }
         }
